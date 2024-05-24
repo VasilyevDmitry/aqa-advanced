fa8e8a109e8502fe97468055b25d7ab694c6bcd7
admin <admin@MacBook-Pro-admin.local>
2024-05-24 14:32:04 +0200

    lessonSevenJS

---
+++ lessonSevenJS/task7L1.js
@@ -0,0 +1,32 @@
+let min = 1
+let max = 100
+
+function rundomNumber (min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let someNum = rundomNumber(min, max)
+
+
+function handleNum(num,numberOdd, numberEven ){
+
+    num % 2 !== 0 ? numberOdd(num) : numberEven(num)
+
+}
+
+
+function getNumberOdd(){
+    console.log(`the number ${someNum} is Odd`);
+
+}
+
+
+function getNumberEven (){
+
+    console.log(`the number ${someNum} is Even`);
+
+}
+
+handleNum(someNum, getNumberOdd, getNumberEven)
